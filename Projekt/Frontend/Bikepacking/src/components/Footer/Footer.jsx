a5d4586ea461ca27b4f90ea90c00323123feabe9
Lukas Randecker <randeckerlukas@gmail.com>
2026-02-20 14:55:11 +0100

    add global gitignore

--- Projekt/Frontend/Bikepacking/src/components/Footer/Footer.jsx
+++ Projekt/Frontend/Bikepacking/src/components/Footer/Footer.jsx
@@ -8,19 +8,15 @@ function Footer() {
         <img src={"../../../IMG/HeroIMG2.jpg"} className="w-full h-full object-cover"/>
         <div className="absolute inset-0 bg-black/70" />
 
-        {/* Footer Content */}
         <div className="absolute inset-0 pt-6 flex items-top md:items-center">
           <div className="max-w-7xl mx-auto px-6 md:px-20 w-full text-white">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-              {/* Branding / Kurztext */}
               <div>
                 <h3 className="text-2xl font-bold py-2 uppercase">Tour. Setup. Packlist. </h3>
                 <p className="text-sm text-gray-300 leading-relaxed">
                   Plan your ride. <br /> Dial in your setup.<br /> Never forget a thing.
                 </p>
               </div>
-
-              {/* Rechtliches */}
               <div>
                 <h4 className="text-lg font-semibold py-2 uppercase">Legal</h4>
                 <ul className="space-y-2 text-sm text-gray-300">
@@ -41,8 +37,6 @@ function Footer() {
                   </li>
                 </ul>
               </div>
-
-              {/* Kontakt */}
               <div className="w-full">
                 <h4 className="text-lg font-semibold py-2 uppercase">Contact</h4>
                 <ul className="space-y-2 text-sm text-gray-300">
